e5c936764ed2eba24ebe96c4579548c0032aed9b
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-13 10:13:46 -0700

    Updates to polybar

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -6,6 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar bar1 -c ~/configs/polybar/polybar.conf &
-polybar topbar1 -c ~/configs/polybar/polybar.conf &
+#polybar bar2 -c ~/configs/polybar/polybar.conf &
+polybar topbar2 -c ~/configs/polybar/polybar.conf &
 
