b0abfa6baa102b3f99b9a72c7dcd46589257bf9f
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-23 08:37:56 -0700

    Rearrange status bar items.

--- statusbar/i3pystatus/i3pystatusconfig.py
+++ statusbar/i3pystatus/i3pystatusconfig.py
@@ -2,6 +2,15 @@ from i3pystatus import Status
 
 status = Status()
 
+# Shows pulseaudio default sink volume
+#
+# Note: requires libpulseaudio from PyPI
+status.register("pulseaudio",
+    format="♪{volume}",)
+
+# Shows your CPU temperature, if you have an Intel CPU
+status.register("temp",
+    format="{temp:.0f}°C",)
 # Displays clock like this:
 # Tue 30 Jul 11:59:46 PM KW31
 #                          ^-- calendar week
@@ -11,10 +20,6 @@ status.register("clock",
 # Shows the average load of the last minute and the last 5 minutes
 # (the default value for format is used)
 # status.register("load")
-
-# Shows your CPU temperature, if you have a Intel CPU
-status.register("temp",
-    format="{temp:.0f}°C",)
 
 # The battery monitor has many formatting options, see README for details
 
@@ -80,29 +85,23 @@ status.register("battery",
 #     # format="Mem:{used}/{total}G[{avail}G]",)
 #     format="AvailMem:{avail}Gb",)
 
-# Shows pulseaudio default sink volume
-#
-# Note: requires libpulseaudio from PyPI
-status.register("pulseaudio",
-    format="♪{volume}",)
-
-# status.register("spotify",
-# 	status={
-#         "pause": "▷",
-#         "play": "▶",
-#         "stop": "◾",
-#     },
-#     format="{title}{status}",)
-
-# Shows mpd status
-# Format:
-# Cloud connected▶Reroute to Remain
-status.register("mpd",
-    format="{title}{status}{album}",
+status.register("spotify",
     status={
         "pause": "▷",
         "play": "▶",
         "stop": "◾",
-    },)
+    },
+    format="{title}{status}",)
+
+# Shows mpd status
+# Format:
+# Cloud connected▶Reroute to Remain
+#status.register("mpd",
+#    format="{title}{status}{album}",
+#    status={
+#        "pause": "▷",
+#        "play": "▶",
+#        "stop": "◾",
+#    },)
 
 status.run()
