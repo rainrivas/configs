8f245088cac7ee40b0a5e3a7d1dae72a2b8e8bbe
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-10-24 16:59:25 -0700

    Remove unsed modules from status bar

--- statusbar/i3pystatus/i3pystatusconfig.py
+++ statusbar/i3pystatus/i3pystatusconfig.py
@@ -18,31 +18,6 @@ status.register("temp",
 status.register("clock",
     format="%a %b %-d | %X | Week %V",)
 
-# Shows the average load of the last minute and the last 5 minutes
-# (the default value for format is used)
-# status.register("load")
-
-# The battery monitor has many formatting options, see README for details
-
-# This would look like this, when discharging (or charging)
-# ↓14.22W 56.15% [77.81%] 2h:41m
-# And like this if full:
-# =14.22W 100.0% [91.21%]
-#
-# This would also display a desktop notification (via D-Bus) if the percentage
-# goes below 5 percent while discharging. The block will also color RED.
-# If you don't have a desktop notification demon yet, take a look at dunst:
-#   http://www.knopwob.org/dunst/
-# status.register("battery",
-#     format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
-#     alert=True,
-#     alert_percentage=5,
-#     status={
-#         "DIS": "↓",
-#         "CHR": "↑",
-#         "FULL": "=",
-#     },)
-
 # This would look like this:
 # Discharging 6h:51m
 status.register("battery",
@@ -55,37 +30,6 @@ status.register("battery",
         "FULL": "FULL",
     },)
 
-# Displays whether a DHCP client is running
-#status.register("runwatch",
-#    name="DHCP",
-#    path="/var/run/dhclient*.pid",)
-
-# Shows the address and up/down state of eth0. If it is up the address is shown in
-# green (the default value of color_up) and the CIDR-address is shown
-# (i.e. 10.10.10.42/24).
-# If it's down just the interface name (eth0) will be displayed in red
-# (defaults of format_down and color_down)
-#
-# Note: the network module requires PyPI package netifaces
-# status.register("network",
-#     interface="eth0",
-#     format_up="{v4cidr}",)
-
-# Note: requires both netifaces and basiciw (for essid and quality)
-#status.register("network",
-#    interface="wlan0",
-#    format_up="{essid} {quality:03.0f}%",
-#	format_down="{essid} WiFi",)
-
-
-# Shows disk usage of /
-# Format:
-# 42/128G [86G]
-# status.register("disk",
-#     path="/",
-#     # format="Mem:{used}/{total}G[{avail}G]",)
-#     format="AvailMem:{avail}Gb",)
-
 status.register("now_playing",
     status={
         "pause": "▷",
@@ -97,16 +41,5 @@ status.register("now_playing",
     on_downscroll=[],
     on_middleclick=[],
     on_rightclick=["player_command", "Stop"],)
-
-# Shows mpd status
-# Format:
-# Cloud connected▶Reroute to Remain
-#status.register("mpd",
-#    format="{title}{status}{album}",
-#    status={
-#        "pause": "▷",
-#        "play": "▶",
-#        "stop": "◾",
-#    },)
 
 status.run()
